9e2c7d99ff631b56a29d9f1df41a4ab666e1ec92
rahulrmsh <rahulramesh13.rr@gmail.com>
2020-10-06 16:48:18 +0800

    app update

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
+      .then((_) {
+    runApp(MainApp());
+  });
+}
+
+class MainApp extends StatefulWidget {
+  @override
+  _MainAppState createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: 'splash',
+      routes: {},
+    );
+  }
+}
